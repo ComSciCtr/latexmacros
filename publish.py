16d5db43416976d35d90523fcc74249a65c43e78
Ryan James <ryangregoryjames@gmail.com>
2016-03-01 15:23:07 -0800

    update publish to inline bbl and remove comments

--- publish.py
+++ publish.py
@@ -38,6 +38,7 @@ TEMPLATE_FILES = [
 
 UNWANTED_EXTENSIONS = [
     'aux',
+    'bbl',
     'bib',
     'blg',
     'brf',
@@ -69,6 +70,8 @@ def publish(tex, inline):
 
     - Produce a `publish` directory with tex files copied over
 
+    - Remove comments
+
     - Factor out tikz code
 
     - Rename images to figure-n.pdf and place them alongside the tex
@@ -81,6 +84,8 @@ def publish(tex, inline):
 
     - Remove `fixme` notes
 
+    - Inline the bbl file
+
     - Remove unneeded files
 
     """
@@ -89,6 +94,9 @@ def publish(tex, inline):
 
     LOG.info('Creating publication version of {}'.format(tex))
     pdir = create_and_populate(tex)
+
+    LOG.info('Removing comments')
+    remove_comments(tex, pdir)
 
     LOG.info('Removing TikZ dependency')
     remove_tikz(tex, pdir)
@@ -105,8 +113,11 @@ def publish(tex, inline):
     LOG.info('Making document final')
     make_final(tex, pdir)
 
+    LOG.info('Inlining the bbl file')
+    inline_bbl(tex, pdir)
+
     LOG.info('Compiling document')
-    compile_tex(tex, pdir)
+    compile_tex(tex, pdir, bibtex=False)
 
     LOG.info('Cleaning directory')
     clean_directory(pdir)
@@ -144,6 +155,19 @@ def find_bib_files(tex):
     bib_files = bib_file_matches.groupdict()['bib_files']
     bib_files = [ fn+'.bib' for fn in bib_files.split(',')]
     return bib_files
+
+
+def remove_comments(tex, cwd):
+    """
+    """
+    comment_regex = r'%.*'
+    with open("{}/{}".format(cwd, tex)) as source:
+        source = source.read()
+
+    source = re.sub(comment_regex, '%', source)
+
+    with open("{}/{}".format(cwd, tex), 'w') as output:
+        output.write(source)
 
 
 def remove_tikz(tex, cwd):
@@ -341,6 +365,28 @@ def make_final(tex, cwd):
         output.write(source)
 
 
+def inline_bbl(tex, cwd):
+    """
+    """
+    regex = re.compile(r'\\bibliography\{.*?\}')
+
+
+    with open("{}/{}.bbl".format(cwd, tex.split('.')[0])) as bbl:
+        bbl = bbl.readlines()
+
+    with open("{}/{}".format(cwd, tex)) as source:
+        source = source.readlines()
+
+    bib = [ i for i, line in enumerate(source) if regex.match(line) ]
+
+    if bib:
+        source = source[:bib[0]] + bbl + source[bib[0]+1:]
+
+        with open("{}/{}".format(cwd, tex), 'w') as output:
+            output.writelines(source)
+
+
+
 def find_authors(sty_file):
     """
     """
@@ -367,13 +413,15 @@ def comment_lines(filename, regexs):
         output.write(source)
 
 
-def compile_tex(tex, cwd):
+def compile_tex(tex, cwd, bibtex=True):
     """
     """
     pdflatex = "pdflatex --shell-escape {}".format(tex)
     bibtex = "bibtex {}".format(tex.split('.')[0])
 
-    cmds = [pdflatex, bibtex, pdflatex, pdflatex]
+    cmds = [pdflatex, pdflatex]
+    if bibtex:
+        cmds = [pdflatex, bibtex] + cmds
 
     for cmd in cmds:
         sarge.capture_both(cmd, cwd=cwd)
